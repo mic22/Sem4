a3bd3af4937f9c08f88ff2e67b6e9b5892feb710
Michał Konieczny <mic22@mic22.info>
2014-06-01 14:12:42 +0200

    wpf_app

---
+++ wpf_app/wpf_app/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_app
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Point> GetData { get; set; }
+        static Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GetData = new ObservableCollection<Point>();
+            GetData.Add(new Point(0, rnd.Next(20)));
+            GetData.Add(new Point(1, rnd.Next(20)));
+            GetData.Add(new Point(2, rnd.Next(20)));
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GetData.Add(new Point(GetData.Last().X + 1, rnd.Next(20)));
+        }
+    }
+}
---
+++ wpf_app/wpf_app/ObservableCollectio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wpf_app
+{
+    class ObservableCollectio
+    {
+    }
+}
