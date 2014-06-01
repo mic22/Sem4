3ab4d4f9f39272d7a49116ee96275b752158eb28
Michał Konieczny <mic22@mic22.info>
2014-06-01 09:48:20 +0200

    SMTP

---
+++ Smtp/Smtp/MailContentDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smtp
+{
+    public class PersonEntry
+    {
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string LastName { get; set; }   
+    }
+    public class MailContentDefinition
+    {
+        public IEnumerable<PersonEntry> People { get; set; }
+        public string Footer { get; set; }
+    }
+}
---
+++ Smtp/Smtp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+
+namespace Smtp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MailContentDefinition mcd;
+        private IEnumerable<string> replaces;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mcd = (MailContentDefinition)XamlServices.Load("data.xaml");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new SmtpClient();
+            foreach (var person in mcd.People)
+            {
+                string completed = Body.Text;
+                completed
+                    .Replace("{Firstname}", person.Firstname)
+                    .Replace("{Lastname}", person.LastName)
+                    .Replace("{Footer}", mcd.Footer);
+
+
+                MailMessage message = new MailMessage("from@ath.pl", person.Email);
+                message.Subject = "testowy";
+                message.Body = completed;
+
+                client.Send(message);
+            }
+        }
+    }
+}
