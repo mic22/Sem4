f0e460e7d17d9e0edc2e3dcd64c7a8c3e321a49e
Michał Konieczny <mic22@mic22.info>
2014-06-01 09:49:04 +0200

    Zaliczenie

--- Zaliczenie/Zaliczenie/Form1.Designer.cs
+++ Zaliczenie/Zaliczenie/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.process = new System.Windows.Forms.Button();
+            this.targetUrl = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.element = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.data_source = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.browser);
+            this.splitContainer1.Size = new System.Drawing.Size(712, 403);
+            this.splitContainer1.SplitterDistance = 186;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.process);
+            this.splitContainer2.Panel1.Controls.Add(this.targetUrl);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer2.Size = new System.Drawing.Size(712, 186);
+            this.splitContainer2.SplitterDistance = 237;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // process
+            // 
+            this.process.Dock = System.Windows.Forms.DockStyle.Top;
+            this.process.Location = new System.Drawing.Point(0, 20);
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(237, 23);
+            this.process.TabIndex = 3;
+            this.process.Text = "Select";
+            this.process.UseVisualStyleBackColor = true;
+            this.process.Click += new System.EventHandler(this.process_Click);
+            // 
+            // targetUrl
+            // 
+            this.targetUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.targetUrl.Location = new System.Drawing.Point(0, 0);
+            this.targetUrl.Name = "targetUrl";
+            this.targetUrl.Size = new System.Drawing.Size(237, 20);
+            this.targetUrl.TabIndex = 2;
+            this.targetUrl.Text = "http://coinsclink.com/zaliczenie/form.html";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.element,
+            this.name,
+            this.data_source});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(471, 186);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // element
+            // 
+            this.element.HeaderText = "Element";
+            this.element.Name = "element";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            // 
+            // data_source
+            // 
+            this.data_source.HeaderText = "Data source";
+            this.data_source.Name = "data_source";
+            this.data_source.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.data_source.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(712, 213);
+            this.browser.TabIndex = 0;
+            this.browser.Url = new System.Uri("", System.UriKind.Relative);
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 403);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button process;
+        private System.Windows.Forms.TextBox targetUrl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn element;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data_source;
     }
 }
 
--- Zaliczenie/Zaliczenie/Form1.cs
+++ Zaliczenie/Zaliczenie/Form1.cs
@@ -12,9 +12,106 @@ namespace Zaliczenie
 {
     public partial class Form1 : Form
     {
+        private HtmlDocument document;
+        private HashSet<HtmlDocument> documentHandlers;
+
+        private IDictionary<HtmlElement, string> elementStyles = new Dictionary<HtmlElement, string>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlDocument htmlDoc = browser.Document;
+
+            document = browser.Document;
+
+            //-- add our handler only once
+            //if (!this.documentHandlers.Contains(document))
+            //{
+                document.MouseOver += new HtmlElementEventHandler(document_MouseOver);
+                document.MouseLeave += new HtmlElementEventHandler(document_MouseLeave);
+
+                mshtml.HTMLDocumentEvents2_Event iEvent;
+                mshtml.IHTMLDocument2 currentDoc = (mshtml.IHTMLDocument2)browser.Document.DomDocument;
+
+                iEvent = (mshtml.HTMLDocumentEvents2_Event)currentDoc;
+                iEvent.onclick += new mshtml.HTMLDocumentEvents2_onclickEventHandler(clickDocumentHandler);
+
+           //     this.documentHandlers.Add(document);
+           // }
+        }
+
+        private void document_MouseLeave(object sender, HtmlElementEventArgs e)
+        {
+            HtmlElement element = e.FromElement;
+            if (this.elementStyles.ContainsKey(element))
+            {
+                string style = this.elementStyles[element];
+                this.elementStyles.Remove(element);
+                element.Style = style;
+            }
+
+            mshtml.HTMLElementEvents2_Event iEvent;
+            iEvent = element.DomElement as mshtml.HTMLElementEvents2_Event;
+            if (iEvent == null)
+                return;
+            iEvent.onclick -= new mshtml.HTMLElementEvents2_onclickEventHandler(clickElementHandler);
+        }
+
+        private void document_MouseOver(object sender, HtmlElementEventArgs e)
+        {
+            HtmlElement element = e.ToElement;
+            if (!this.elementStyles.ContainsKey(element))
+            {
+                string style = element.Style;
+                this.elementStyles.Add(element, style);
+                element.Style = style + "; border: 1px dotted lightblue; cursor: crosshair;";
+                this.Text = element.TagName ?? "(no id)";
+            }
+
+            mshtml.HTMLElementEvents2_Event iEvent;
+            iEvent = element.DomElement as mshtml.HTMLElementEvents2_Event;
+            if (iEvent == null)
+                return;
+            iEvent.onclick += new mshtml.HTMLElementEvents2_onclickEventHandler(clickElementHandler);
+        }
+
+        private bool clickDocumentHandler(mshtml.IHTMLEventObj e)
+        {
+            mshtml.IHTMLElement element = (mshtml.IHTMLElement) e.srcElement;
+
+            string euid = DateTime.Now.ToString("yyyymmddhhssfff");
+            element.setAttribute("data-euid", euid);
+            element.setAttribute("style", "background:red;");
+            dataGridView1.Rows.Add(euid, element.tagName, "");
+
+            this.Text = e.srcElement.toString();
+            e.cancelBubble = true;
+            e.returnValue = false;
+            return false;
+        }
+
+        private bool clickElementHandler(mshtml.IHTMLEventObj pEvtObj)
+        {
+            mshtml.IHTMLElement element = (mshtml.IHTMLElement)pEvtObj.srcElement;
+            this.Text = element.toString();
+            pEvtObj.cancelBubble = true;
+            pEvtObj.returnValue = false;
+
+            return false;
+        }
+
+        private void process_Click(object sender, EventArgs e)
+        {
+            browser.Url = new Uri(targetUrl.Text);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
