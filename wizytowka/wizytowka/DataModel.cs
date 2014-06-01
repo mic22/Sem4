675774e5bb1f0ac222e1c384359965e3719db430
Michał Konieczny <mic22@mic22.info>
2014-06-01 14:13:37 +0200

    wizytówka

---
+++ wizytowka/wizytowka/DataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wizytowka
+{
+    class Person
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+ 
+        public Person(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+    }
+}
